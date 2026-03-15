62e2b21bf31fdc7ce9d03f9cfd3e059c90203bf5
Robert Kotrutsa <tr3asur3island@gmail.com>
2026-03-15 16:40:57 +0100

    refactor(notifications): move UI updates to main thread and clean up code

--- H2O/VC/NotificationsVC.swift
+++ H2O/VC/NotificationsVC.swift
@@ -33,22 +33,18 @@ class NotificationsVC: UIViewController {
     
     @IBAction func notificationSwitcherAction(_ sender: UISwitch) {
         if sender.isOn {
-            print("User turned on Reminders")
             NotificationManager.shared.requestPermission { isAllowed in
-                DispatchQueue.main.async {
-                    guard isAllowed else {
-                        print("User turned off Notifications")
+                guard isAllowed else {
+                    DispatchQueue.main.async {
                         self.notificationSwitcherOutlet.isOn = false
-                        UserDefaults.standard.set(false, forKey: "waterReminder")
-                        return
                     }
-                    print("User turned on Notifications")
-                    UserDefaults.standard.set(true, forKey: "waterReminder")
-                    NotificationManager.shared.sendNotification()
+                    UserDefaults.standard.set(false, forKey: "waterReminder")
+                    return
                 }
+                UserDefaults.standard.set(true, forKey: "waterReminder")
+                NotificationManager.shared.sendNotification()
             }
         } else {
-            print("User turned off Reminders")
             UserDefaults.standard.set(sender.isOn, forKey: "waterReminder")
             NotificationManager.shared.removeNotification()
         }
