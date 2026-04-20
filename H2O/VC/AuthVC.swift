b670c20268cfa69200f3d89790d14c489dbf1550
Robert Kotrutsa <tr3asur3island@gmail.com>
2026-04-20 13:12:10 +0200

    feat: configure text fields UI in AuthVC

--- H2O/VC/AuthVC.swift
+++ H2O/VC/AuthVC.swift
@@ -9,9 +9,32 @@ import UIKit
 
 class AuthVC: UIViewController {
 
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var loginButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        setup(emailTextField)
+        setup(nameTextField)
+        setup(passwordTextField)
 
     }
+    
+    @IBAction func login(_ sender: UIButton) {
+        print(#function)
+    }
+    
+    private func setup(_ textField: UITextField) {
+        textField.layer.cornerRadius = 10
+        textField.borderStyle = .none
+        textField.layer.borderWidth = 1
+        textField.layer.borderColor = UIColor.gray.cgColor
+        textField.clipsToBounds = true
+    }
+    
 
 }
