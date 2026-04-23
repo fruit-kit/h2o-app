34d62de046fafa22e51b98888fe6325bd4c9c187
Robert Kotrutsa <tr3asur3island@gmail.com>
2026-04-23 18:43:26 +0200

    feat: show correct screen based on auth state and add login/signup toggle

--- H2O/App/SceneDelegate.swift
+++ H2O/App/SceneDelegate.swift
@@ -6,12 +6,13 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
-
+    
     var window: UIWindow?
-
-
+    
+    
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         DrinkManager.shared.loadHistory()
         DrinkManager.shared.checkDate()
@@ -20,40 +21,49 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         
         let window = UIWindow(windowScene: windowScene)
         
-        let authVC = AuthVC(nibName: "AuthVC", bundle: Bundle.main)
-        window.rootViewController = authVC
+        var rootVC: UIViewController
         
+        if Auth.auth().currentUser == nil {
+            let authVC = AuthVC(nibName: "AuthVC", bundle: nil)
+            rootVC = authVC
+        } else {
+            let storyboard = UIStoryboard(name: "Main", bundle: nil)
+            let homeVC = storyboard.instantiateViewController(identifier: "HomeVC") as! HomeVC
+            rootVC = homeVC
+        }
+        window.rootViewController = rootVC
         self.window = window
         window.makeKeyAndVisible()
+        
     }
-
+    
     func sceneDidDisconnect(_ scene: UIScene) {
         // Called as the scene is being released by the system.
         // This occurs shortly after the scene enters the background, or when its session is discarded.
         // Release any resources associated with this scene that can be re-created the next time the scene connects.
         // The scene may re-connect later, as its session was not necessarily discarded (see `application:didDiscardSceneSessions` instead).
     }
-
+    
     func sceneDidBecomeActive(_ scene: UIScene) {
         // Called when the scene has moved from an inactive state to an active state.
         // Use this method to restart any tasks that were paused (or not yet started) when the scene was inactive.
     }
-
+    
     func sceneWillResignActive(_ scene: UIScene) {
         // Called when the scene will move from an active state to an inactive state.
         // This may occur due to temporary interruptions (ex. an incoming phone call).
     }
-
+    
     func sceneWillEnterForeground(_ scene: UIScene) {
         DrinkManager.shared.checkDate()
     }
-
+    
     func sceneDidEnterBackground(_ scene: UIScene) {
         // Called as the scene transitions from the foreground to the background.
         // Use this method to save data, release shared resources, and store enough scene-specific state information
         // to restore the scene back to its current state.
     }
-
-
+    
+    
 }
 
--- H2O/VC/AuthVC.swift
+++ H2O/VC/AuthVC.swift
@@ -9,10 +9,29 @@ import UIKit
 
 class AuthVC: UIViewController {
 
+    private var signup: Bool = true {
+        didSet {
+            if signup {
+                label.text = "Create an account"
+                nameTextField.isHidden = false
+                statusLabel.text = "Already have an account?"
+                passwordTextField.placeholder = "Create your password"
+                loginButton.setTitle("Log In", for: .normal)
+            } else {
+                label.text = "Login"
+                nameTextField.isHidden = true
+                passwordTextField.placeholder = "Enter your password"
+                statusLabel.text = "Don't have an account yet?"
+                loginButton.setTitle("Sign Up", for: .normal)
+            }
+        }
+    }
+    
     @IBOutlet weak var label: UILabel!
     @IBOutlet weak var emailTextField: UITextField!
     @IBOutlet weak var nameTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var statusLabel: UILabel!
     @IBOutlet weak var loginButton: UIButton!
     
     override func viewDidLoad() {
@@ -24,8 +43,8 @@ class AuthVC: UIViewController {
 
     }
     
-    @IBAction func login(_ sender: UIButton) {
-        print(#function)
+    @IBAction func switchLogin(_ sender: UIButton) {
+        signup.toggle()
     }
     
     private func setup(_ textField: UITextField) {
